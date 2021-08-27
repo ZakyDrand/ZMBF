6cdc1f01c685280d334399577388cadb4eee61d6
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2021-08-27 14:05:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-import os
+import sys, os, subprocess
 
-os.system("git pull")
-print(" [*] maaf script masih dalam pembaruan \n [*] silahkan menunggu sampai pembaruan selesai \n [*] terima kasih...")
+
+if sys.version[0:3] != "3.9":
+  sys.exit("[!] Anda harus menggunakan versi python 3.9, versi python anda sekarang : "+sys.version[0:3])
+
+if __name__ == "__main__":
+  try:
+    os.system("git pull")
+    __import__("zmbf").cekey()
+  except Exception:
+      os.system("git pull")
+      __import__("zmbf").cekey()
