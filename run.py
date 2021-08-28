4a2c1800904aff6cb572c686d0e75814059779e2
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2021-08-28 15:24:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 
 os.system("git pull")
 print(" [*] script saat ini sedang maintenance \n [*] silahkan gunakan script sementara \n [*] untuk script sementara silahkan hubungin wa saya \n [*] wa : 082329761867")
